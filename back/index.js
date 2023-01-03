4e09b7abb12aaa6d3bc991cc9229b63f6af4c605
Slawomir <s.sulbinski@sprzeglo.com.pl>
2023-01-03 16:08:21 +0100

    mongo update

--- back/index.js
+++ back/index.js
@@ -9,14 +9,23 @@ async function run() {
 		// Connect the client to the server (optional starting in v4.7)
 		await client.connect();
 		// Establish and verify connection
-		await client.db('sksujmi').command({ ping: 1 });
-		console.log('Connected successfully to server');
-	} finally {
-		// Ensures that the client will close when you finish/error
-		await client.close();
+		const sksujmi = client.db('sksujmi');
+		sksujmi
+			.collection('conversation')
+			.find()
+			.toArray(function (err, res) {
+				if (err) {
+					console.log(err);
+					return;
+				}
+				console.log(res);
+			});
+	} catch (e) {
+		console.log(e);
 	}
 }
-run().catch(console.dir);
+run();
+// run().catch(console.dir);
 const server = http.createServer(function (request, response) {
 	console.log(new Date() + ' Received request for ' + request.url);
 	response.writeHead(404);
