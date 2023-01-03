6de015730d175f2faeb50a69e399961afb165f6d
Tomasz Bałdyga <tomaszbaldyga3@gmail.com>
2023-01-03 16:06:43 +0100

    ekran logowania

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={darkTheme}>
+        <Box className="login-card">
+          <h2 className="login-header">Sksujmi</h2>
+          <TextField required className="login-input" label="Login" />
+          <TextField required className="login-input" label="Password" />
+          <div className="login-actions">
+            <Button variant="contained">Login as guest</Button>
+            <Button variant="contained" disabled>
+              Login
+            </Button>
+          </div>
+        </Box>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
