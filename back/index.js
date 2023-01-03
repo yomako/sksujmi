97f2f99704cf54e2d542ecd7617706aad37de134
Slawomir <s.sulbinski@sprzeglo.com.pl>
2023-01-03 15:32:23 +0100

    mongodb

--- back/index.js
+++ back/index.js
@@ -1,6 +1,22 @@
 import { server as WebSocketServer } from 'websocket';
 import http from 'http';
-
+import { MongoClient } from 'mongodb';
+const uri = 'mongodb://localhost:27017/?maxPoolSize=20&w=majority';
+// // Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+	try {
+		// Connect the client to the server (optional starting in v4.7)
+		await client.connect();
+		// Establish and verify connection
+		await client.db('sksujmi').command({ ping: 1 });
+		console.log('Connected successfully to server');
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
 const server = http.createServer(function (request, response) {
 	console.log(new Date() + ' Received request for ' + request.url);
 	response.writeHead(404);
